6a9d60dad799264a9ff2bfa8c4701e7937abebbe
PRASANNA <108861577+aravindh-6@users.noreply.github.com>
2025-05-28 15:15:54 +0530

    Add files via upload

--- views.py
+++ views.py
@@ -1,6 +1,18 @@
+
 from django.shortcuts import render
+from django.http import HttpResponse
+
+def calculate():
+    x = 20
+    y = 300
+    return y
+
+def disc(request):
+    x = calculate()
+    return render(request,'main.html',{'name':'guru','cal':x})
 
 
-# Create your views here.
-def calc(request):
-    return render(request,'main.html')
+    
+    
+
+
