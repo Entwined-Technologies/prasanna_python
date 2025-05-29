32008106ffa3a26d8805044057bc8dabc3a75345
PRASANNA <108861577+aravindh-6@users.noreply.github.com>
2025-05-29 12:42:46 +0530

    Add files via upload

--- models.py
+++ models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class user(models.Model):
+    fullname = models.CharField(max_length=100,blank=True)
+    email = models.EmailField(max_length=100,blank=True)
+    password = models.CharField(max_length=100,blank=True)
+    username = models.CharField(max_length=100,blank=True)
+
+
+
+
--- views.py
+++ views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 
-
-# Create your views here.
-def calc(request):
+def main(request):
     return render(request,'main.html')
+
+def login(request):
+    return render(request,'login.html')
+
+def register(request):
+    return render(request,'register.html')
+
+
